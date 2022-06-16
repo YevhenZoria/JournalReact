8da2aee933e348f5e0b9dba63a70bc2db890c8bd
Yevhen <yevhen.zoria@outlook.com>
2022-06-16 13:25:40 +0300

    +1

--- src/App.js
+++ src/App.js
@@ -4,20 +4,48 @@ import Counter from "./components/Counter";
 import './styles/App.css';
 import CallItem from './components/CallItem'
 import CallList from "./components/CallList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/Input/MyInput";
 
 function App() {
-  const [calls, setCalls] = useState([
-    { id: 1, Problem: 'Перша проблема', Remedy: 'Рішення' },
-    { id: 2, Problem: 'Друга проблема', Remedy: 'Рішення' },
-    { id: 3, Problem: 'Третя проблема', Remedy: 'Рішення' },
-    { id: 4, Problem: 'Четверта проблема', Remedy: 'Рішення' },
-    { id: 5, Problem: "П'ята проблема", Remedy: 'Рішення' },
-    { id: 6, Problem: 'Шоста проблема', Remedy: 'Рішення' },
+  const [callsTP, setCallsTP] = useState([
+    { id: 1, Problem: 'Перша проблема на традиційному процесі', Remedy: 'Рішення' },
+    { id: 2, Problem: 'Друга проблема на традиційному процесі', Remedy: 'Рішення' },
+    { id: 3, Problem: 'Третя проблема на традиційному процесі', Remedy: 'Рішення' },
+    { id: 4, Problem: 'Четверта проблема на традиційному процесі', Remedy: 'Рішення' },
+    { id: 5, Problem: "П'ята проблема на традиційному процесі", Remedy: 'Рішення' },
+    { id: 6, Problem: 'Шоста проблема на традиційному процесі', Remedy: 'Рішення' },
   ])
+  const [callsMP, setCallsMP] = useState([
+    { id: 1, Problem: 'Перша проблема на сучасному процесі', Remedy: 'Рішення' },
+    { id: 2, Problem: 'Друга проблема на сучасному процесі', Remedy: 'Рішення' },
+    { id: 3, Problem: 'Третя проблема на сучасному процесі', Remedy: 'Рішення' },
+    { id: 4, Problem: 'Четверта проблема на сучасному процесі', Remedy: 'Рішення' },
+    { id: 5, Problem: "П'ята проблема на сучасному процесі", Remedy: 'Рішення' },
+    { id: 6, Problem: 'Шоста проблема на сучасному процесі', Remedy: 'Рішення' },
+  ])
+
+  const [problem, setProblem] = useState('')
+
+  const addNewCall = (e) => {
+    e.preventDefault()
+    console.log(problem)
+  }
 
   return (
     <div className="App">
-      <CallList calls={calls} />
+      <form>
+        <MyInput
+          value={problem}
+          onChange = {e => setProblem(e.target.value)} 
+          type="text" 
+          placeholder="Опис проблеми" />
+        <MyInput type="text" placeholder="Метод вирішення" />
+        <MyButton onClick={addNewCall}>Створити виклик!</MyButton>
+      </form>
+      <CallList calls={callsTP} title="Перелік викликів ТРАДИЦІЙНИЙ ПРОЦЕС" />
+      <CallList calls={callsMP} title="Перелік викликів СУЧАСНИЙ ПРОЦЕС" />
+
 
     </div>
   );
--- src/components/CallList.jsx
+++ src/components/CallList.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import CallItem from './CallItem'
 
-const CallList = ({calls}) => {
+const CallList = ({calls, title}) => {
     return (
         <div>
-            <h1 style={{ textAlign: "center" }}>Перелік викликів</h1>
+            <h1 style={{ textAlign: "center" }}>
+                {title}
+                </h1>
             {calls.map((call) =>
              <CallItem call={call} key={call.id} />
             )}
